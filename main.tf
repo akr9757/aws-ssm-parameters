a3226768d346d23abd4d11df0794b768b81002d9
akr9757 <anilkumareddy9757@gmail.com>
2024-08-12 16:08:54 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "foo" {
+resource "aws_ssm_parameter" "string" {
   for_each = var.parameters_store
   name  = each.key
   type  = "String"
@@ -7,7 +7,7 @@ resource "aws_ssm_parameter" "foo" {
 }
 
 
-resource "aws_ssm_parameter" "foo" {
+resource "aws_ssm_parameter" "secrets" {
   for_each = var.secrets
   name  = each.key
   type  = "SecureString"
